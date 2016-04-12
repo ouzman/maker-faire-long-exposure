b3a794c18cc1d016a6e0baebca3e0b17ae263188
Oğuzhan UZMAN <ouzman@anadolu.edu.tr>
2016-04-12 12:07:07 +0300

    s3 upload az kaldi. env kullanmak gerek.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var express = require('express'),
-    Jimp = require("jimp");
+    Jimp = require('jimp'),
+    aws = require('aws-sdk');
 
 var app = express();
+
+var s3Options = {
+    accessKeyId: 'xxx',
+    secretAccessKey: 'yyy',
+    // region: 'US Standard',
+    endpoint: 's3.amazonaws.com'
+};
+var awsS3Client = new aws.S3(s3Options);
+
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
@@ -9,46 +19,68 @@ app.listen(3000, function() {
 
 app.get('/photo', function(req, res) {
     var takePhoto = new run_cmd(
-        'gphoto2', 
-        ['--set-config', 'shutterspeed=46', '--capture-image-and-download', '--force-overwrite'],
+        'gphoto2', ['--set-config', 'shutterspeed=46', '--capture-image-and-download', '--force-overwrite'],
         commandOutputAppender,
-        cbRunCommandResizeAndManupulateTheImage);
+        function() {
+            cbRunCommandResizeAndManupulateTheImage(res, takePhoto);
+        });
 });
 
 app.get('/test', function(req, res) {
     var testCommand = new run_cmd(
-        'cat', 
-        ['mock'],
+        'cat', ['mock'],
         commandOutputAppender,
-        cbRunCommandResizeAndManupulateTheImage);
+        function() {
+            cbRunCommandResizeAndManupulateTheImage(res, testCommand);
+        });
 });
 
 function commandOutputAppender(commander, buffer) {
     commander.stdout += buffer.toString();
 }
 
-function cbRunCommandResizeAndManupulateTheImage() {
-    var result = takePhoto.stdout.replace('undefined', '');
+function cbRunCommandResizeAndManupulateTheImage(res, commander) {
+    var result = commander.stdout.replace('undefined', '');
     resizePhoto(
         parsePathOfPhoto(result)[2],
         function(err, image) {
-            if (err) {
-                console.error(err);
-                res.send('error');
-            } else {
-                var imgSrc = 'data:' + Jimp.MIME_JPEG + ';base64,' + new Buffer(image, 'binary').toString('base64');
-                res.send('<html><head></head><body><img style="width: 40%;" src="' + imgSrc + '" ></body></html>');
-            }
+            cbResizePhotoAddToHtml(res, err, image);
         });
 }
 
-function cbResizePhotoAddToHtml(err, image) {
+function addToHtml(image) {
+    var imgSrc = 'data:' + Jimp.MIME_JPEG + ';base64,' + new Buffer(image, 'binary').toString('base64');
+    res.send('<html><head></head><body><img style="width: 40%;" src="' + imgSrc + '" ></body></html>');
+}
+
+function sendToS3(image) {
+
+    var params = {
+        Bucket: 'maker-faire-long-exposure',
+        Key: '123',
+        ACL: 'public-read',
+        Body: image,
+        ContentType: Jimp.MIME_JPEG,
+    };
+    awsS3Client.putObject(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data); // successful response
+    });
+
+
+
+
+    var imgSrc = 'data:' + Jimp.MIME_JPEG + ';base64,' + new Buffer(image, 'binary').toString('base64');
+    res.send('<html><head></head><body><img style="width: 40%;" src="' + imgSrc + '" ></body></html>');
+}
+
+function cbResizePhotoAddToHtml(res, err, image) {
     if (err) {
         console.error(err);
         res.send('error');
     } else {
-        var imgSrc = 'data:' + Jimp.MIME_JPEG + ';base64,' + new Buffer(image, 'binary').toString('base64');
-        res.send('<html><head></head><body><img style="width: 40%;" src="' + imgSrc + '" ></body></html>');
+        sendToS3(image);
+        addToHtml(image);
     }
 }
 
